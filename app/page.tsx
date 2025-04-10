52ef0a2cc53c4e9dfc3eb52c1fa30e6fcc9ccb9d
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2025-04-10 18:55:28 +0530

    env changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-import { HeroSection } from '../components/HeroSection';
-
-export default function Home() {
-  return (
-    <div className="flex flex-col">
-      <HeroSection />
-    </div>
-  );
-}
+const page = () => {
+  return <div>page</div>;
+};
+export default page;
